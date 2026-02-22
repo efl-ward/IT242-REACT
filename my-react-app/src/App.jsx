ded1b73b0ae9671af8332f6e2af02441c7f86ec4
Eduard Serna <elserna@student.apc.edu.ph>
2026-02-22 18:05:18 +0800

    bisaya greetings

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,6 +2,8 @@ function App() {
   return (
     <div className="App">
       <h1>Hello Kilibutan! ;-; </h1>
+      <h3>Maayong adlaw sa inyong tanan! <h3/>
+      <h2>We are on a learning journey.....<h2/>
     </div>
   );
 }
